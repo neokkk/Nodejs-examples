b53c4f931286945462c35dff53b6855e0cc59138
neokkk <seven3126@gmail.com>
2019-04-18 01:09:46 +0900

    Node.js examples practice
    opentutorials web-N Node.js

---
+++ examples/1_conditional.js
@@ -0,0 +1,8 @@
+const args = process.argv;
+
+if (args[2] === '1')
+    console.log('C1');
+else 
+    console.log('C2');
+
+console.log('D')
--- examples/1_main.js
+++ examples/1_main.js
@@ -2,43 +2,48 @@ const http = require('http'),
       fs = require('fs'),
       url = require('url');
 
-const app = http.createServer(function(req, res) {
+const app = http.createServer((req, res) => {
     let _url = req.url;
-    const queryData = url.parse(_url, true).query;
+    const queryData = url.parse(_url, true).query,
+          pathname = url.parse(_url, true).pathname;
+
     let title = queryData.id;
 
-    if (_url === '/')
+    if (pathname === '/') {
+      if (title === undefined)
         title = 'Welcome';
 
-    if (_url === '/favicon.ico')
-        return res.writeHead(404);
+      fs.readFile(`1_${title}.html`, 'utf-8', (err, data) => {
+        if (data === undefined)
+          data = 'Hello, Node.js!';
 
-    res.writeHead(200);
-    fs.readFile(`1_${title}.html`, 'utf-8', (err, data) => {
-      const template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="?id=HTML">HTML</a></li>
-          <li><a href="?id=CSS">CSS</a></li>
-          <li><a href="?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${data}</p>
-      </body>
-      </html>
-      `;
-
-      res.end(template);
-    });
-
-    
+        const template = `
+          <!doctype html>
+          <html>
+          <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="?id=HTML">HTML</a></li>
+              <li><a href="?id=CSS">CSS</a></li>
+              <li><a href="?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${data}</p>
+          </body>
+          </html>
+          `;
+        
+        res.writeHead(200);
+        res.end(template);
+      });  
+    } else {
+      res.writeHead(404);
+      res.end('Not Found');
+  }             
 });
 
 app.listen(3000);
