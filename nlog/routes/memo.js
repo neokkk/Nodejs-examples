6d75817f10f50ea7a5b06e76f83a889a87d60d7e
neokkk <seven3126@gmail.com>
2019-05-07 00:30:36 +0900

    nlog memo CRUD
    ## error
    
    modify, memo_id crash
    
    ## update
    
    order by recent, scroll fix ...

--- nlog/routes/memo.js
+++ nlog/routes/memo.js
@@ -1,48 +1,66 @@
-const express = require('express'),
-      mysql = require('mysql');
-      router = express.Router();
+const express = require("express"),
+  mysql = require("mysql"),
+  dateFormat = require("dateformat"),
+  path = require("path"),
+  router = express.Router();
 
 /* MySQL DB connect */
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'test'
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "test"
 });
 db.connect();
 
+let memo_id = 0;
+
 /* GET memo listing. */
-router.get('/', (req, res, next) => {
-    db.query('SELECT * FROM t_n_memo', (err, result) => {
-        if (err) throw err;
-        if (result.length === 0)
-            res.render('memo/input');
-        else 
-            res.render('memo/list', { result });
-    });
+router.get("/", (req, res, next) => {
+  db.query("SELECT * FROM t_n_memo", (err, result) => {
+    if (err) throw err;
+
+    memo_id = result.length + 1;
+    result.memo_date = dateFormat(result.memo_date, "longDate");
+
+    res.render("memo/form", { result, memo_id });
+  });
 });
 
 // create
-router.post('/create', (req, res, next) => {
-    const postData = req.body;
+router.post("/create", (req, res, next) => {
+  const postData = req.body;
 
-    if (postData.memo_input !== '') {
-        db.query('INSERT INTO t_n_memo (memo_text, memo_user, memo_date) VALUES (?, ?, Now())',
-            [`${postData.memo_input}`, ''], (err, result) => {
-                if (err) throw err;
-        });
-        res.redirect('/');
-    }
+  if (postData.memo_input !== "") {
+    db.query(
+      "INSERT INTO t_n_memo (memo_id, memo_text, memo_user, memo_date) VALUES (?, ?, ?, Now())",
+      [`${postData.memo_id}`, `${postData.memo_input}`, ""],
+      (err, result) => {
+        if (err) throw err;
+      }
+    );
+    res.redirect("/memo");
+  } else {
+    res.redirect("/memo");
+  }
 });
+
+// modify
+router.get("/modify/:modify_id", (req, res, next) => {});
 
 // update
-router.post('/update/:update_id', (req, res, next) => {
-
-});
+router.post("/update", (req, res, next) => {});
 
 // delete
-router.post('/delete/:delete_id', (req, res, next) => {
+router.get("/delete/:delete_id", (req, res, next) => {
+  const tmp = path.parse(req.params.delete_id).base;
+  const deleteId = tmp.slice(1);
 
+  db.query(`DELETE FROM t_n_memo WHERE memo_id=${deleteId}`, (err, result) => {
+    if (err) throw err;
+
+    res.redirect("/memo");
+  });
 });
 
 module.exports = router;
